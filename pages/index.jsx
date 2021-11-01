c8c6852147212eb9eb00e4dd7f8747a90a6f6a42
William Koerich <williamkoerich17@gmail.com>
2021-11-01 14:34:57 -0400

    feat: changed to render board component

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+import Board from '../src/Components/Board'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Board />
+    </div>
+  )
+}
