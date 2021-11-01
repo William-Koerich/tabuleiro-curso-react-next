91c0e608190cc88af3c57a512ae50abd5e1849a7
William Koerich <williamkoerich17@gmail.com>
2021-11-01 14:31:56 -0400

    feat: created board component

---
+++ src/Components/Board.jsx
@@ -0,0 +1,16 @@
+import Line from './Line'
+
+export default function Board(props) {
+  return (
+    <div>
+      <Line />
+      <Line outline />
+      <Line />
+      <Line outline />
+      <Line />
+      <Line outline />
+      <Line />
+      <Line outline />
+    </div>
+  )
+}
